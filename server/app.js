c26103c19fa5fa388526b4b2ab702f3f8852d567
Maxim <m.shakurov99@mail.ru>
2019-06-02 23:44:07 +0400

    ver1.1.6

--- server/app.js
+++ server/app.js
@@ -4,13 +4,13 @@ const http = require('http');
 var port = 3000;
 const fs = require('fs');
 
+app.use(express.static('public'))
+    // app.get('/', function(req, res) {
 
-app.get('/', function(req, res) {
-
-    // let mySite = fs.readFileSync('E:\\SisAn\\system-analysis-project\\public');
-    // console.log(mySite);
-    // res.end(mySite);
-});
+//     // let mySite = fs.readFileSync('E:\\SisAn\\system-analysis-project\\public');
+//     // console.log(mySite);
+//     // res.end(mySite);
+// });
 
 app.listen(port, function() {
     console.log('Example app listening on port 3000!');
